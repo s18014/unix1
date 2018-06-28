8c2d12fc4cf09dec064fc740851f92f82354c69a
s18014 <s18014@std.it-college.ac.jp>
2018-06-28 11:11:29 +0900

    062804.shを修正

--- 0628/062804/062804.sh
+++ 0628/062804/062804.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk -F, '{print $7}' personal_infomation.csv | sort | count 1 1 | sort -k2 -nr | head -n 5
+awk -F, '{print $7}' personal_infomation.csv | sed 1,1d | sort | count 1 1 | sort -k2 -nr | head -n 5
